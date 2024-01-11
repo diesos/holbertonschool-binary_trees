99ddf45f59446e5fa9f42efac61ee713b271eaf5
Diesos <ozturk@live.Fr>
2024-01-11 14:47:59 +0100

    Fix memory allocation bug and add missing comment

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,10 +11,11 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
-	if (new_node == NULL)
-		return (0);
+
 
 	new_node = malloc(sizeof(binary_tree_t));
+		if (new_node == NULL)
+		return (0);
 	new_node->parent = parent;
 	new_node->n = value;
 	new_node->left = NULL;
--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+
 /**
 * binary_tree_insert_left - inserts a node as the left-child of another node
 * insère un nœud en tant qu'enfant gauche d'un autre nœud
@@ -6,6 +7,7 @@
 * @value: value to store in the new node
 * Return: pointer to the created node, or NULL on failure or if parent is NULL
 */
+
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_left_node;
